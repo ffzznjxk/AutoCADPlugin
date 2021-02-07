f9b0a1cd44b3475a12ba0ae0edb346fe2924286f
phyld <phyld030@outlook.com>
2021-02-08 01:27:08 +0800

    init

---
+++ AutoCADPlugin/EntityTools.cs
@@ -0,0 +1,51 @@
+﻿using Autodesk.AutoCAD.DatabaseServices;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCADPlugin
+{
+    class EntityTools
+    {
+        /// <summary>
+        /// 添加实体
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="entity">实体</param>
+        /// <returns>对象Id</returns>
+        public static ObjectId AddNewEntity(Database db, Entity entity)
+        {
+            //开启事务处理
+            Transaction tr = db.TransactionManager.StartTransaction();
+            //打开块表
+            BlockTable bt = (BlockTable)tr.GetObject(db.BlockTableId, OpenMode.ForRead);
+            //打开块表记录
+            BlockTableRecord btr = (BlockTableRecord)tr.GetObject(bt[BlockTableRecord.ModelSpace], OpenMode.ForWrite);
+            //添加对象到块表记录，返回Id
+            var id = btr.AppendEntity(entity);
+            //
+            btr.DowngradeOpen();
+            tr.AddNewlyCreatedDBObject(entity, true);
+            tr.Commit();
+            return id;
+        }
+
+        /// <summary>
+        /// 添加实体组
+        /// </summary>
+        /// <param name="db">图形数据库</param>
+        /// <param name="entitys">多个实体</param>
+        /// <returns>对象Id</returns>
+        public static ObjectId[] AddNewEntity(Database db, params Entity[] entitys)
+        {
+            ObjectId[] ids = new ObjectId[entitys.Length];
+            for (int i = 0; i < entitys.Length; i++)
+            {
+                ids[i] = AddNewEntity(db, entitys[i]);
+            }
+            return ids;
+        }
+    }
+}
---
+++ AutoCADPlugin/Plugin.cs
@@ -0,0 +1,126 @@
+﻿using Autodesk.AutoCAD.ApplicationServices;
+using Autodesk.AutoCAD.DatabaseServices;
+using Autodesk.AutoCAD.EditorInput;
+using Autodesk.AutoCAD.Geometry;
+using Autodesk.AutoCAD.Runtime;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AutoCADPlugin
+{
+    public class Plugin
+    {
+        [CommandMethod("Hello")]
+        public void HelloWorld()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            ed.WriteMessage("Hello world!");
+        }
+
+        /// <summary>
+        /// 画线
+        /// </summary>
+        [CommandMethod("DrwaLine")]
+        public void DrawLine()
+        {
+            //
+            Point3d pt1 = new Point3d(100, 100, 0);
+            Point3d pt2 = new Point3d(200, 200, 0);
+            Line l = new Line(pt1, pt2);
+            var pt3 = new Point3d(200, 100, 0);
+            var pt4 = new Point3d(300, 200, 0);
+            Line l2 = new Line(pt3, pt4);
+            Database db = HostApplicationServices.WorkingDatabase;
+            var ids = EntityTools.AddNewEntity(db, l, l2);
+        }
+
+
+        [CommandMethod("PDL")]
+        public void PromptDrawLine()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            PromptPointOptions ppo = new PromptPointOptions("\n指定起点");
+            PromptPointResult ppr = ed.GetPoint(ppo);
+            if (ppr.Status == PromptStatus.OK)
+            {
+                Point3d ptStart = ppr.Value;
+                ppo = new PromptPointOptions("\n指定终点");
+                ppr = ed.GetPoint(ppo);
+                if (ppr.Status == PromptStatus.OK)
+                {
+                    Point3d ptEnd = ppr.Value;
+                    Line line = new Line(ptStart, ptEnd);
+
+                    Database db = HostApplicationServices.WorkingDatabase;
+                    var id = EntityTools.AddNewEntity(db, line);
+                }
+            }
+        }
+
+        [CommandMethod("PDML")]
+        public void PromptDrawMultLine()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            PromptPointOptions ppo = new PromptPointOptions("\n指定起点");
+            PromptPointResult ppr = ed.GetPoint(ppo);
+
+            if (ppr.Status == PromptStatus.OK)
+            {
+                Point3d ptStart = ppr.Value;
+                ppo = new PromptPointOptions("\n指定终点");
+                ppr = ed.GetPoint(ppo);
+
+                while (ppr.Status == PromptStatus.OK)
+                {
+                    Point3d ptEnd = ppr.Value;
+                    Line line = new Line(ptStart, ptEnd);
+
+                    Database db = HostApplicationServices.WorkingDatabase;
+                    var id = EntityTools.AddNewEntity(db, line);
+
+                    ptStart = ptEnd;
+                    ppo = new PromptPointOptions("\n指定下一点");
+                    ppr = ed.GetPoint(ppo);
+                }
+            }
+        }
+
+        [CommandMethod("PDPL")]
+        public void PromptDrawPolyLine()
+        {
+            Editor ed = Application.DocumentManager.MdiActiveDocument.Editor;
+            PromptPointOptions ppo = new PromptPointOptions("\n指定起点");
+            PromptPointResult ppr = ed.GetPoint(ppo);
+
+            List<Point3d> pts = new List<Point3d>();
+            while (ppr.Status == PromptStatus.OK)
+            {
+                Point3d pt = ppr.Value;
+                pts.Add(pt);
+                ppo = new PromptPointOptions("\n指定下一点");
+                ppr = ed.GetPoint(ppo);
+            }
+            if (pts.Count > 1)
+            {
+
+                Polyline pl = new Polyline();
+                for (int i = 0; i < pts.Count; i++)
+                {
+                    Point3d pt = pts[i];
+                    pl.AddVertexAt(i, new Point2d(pt.X, pt.Y), 0, 0, 0);
+                }
+                pl.Closed = true;
+
+                Database db = HostApplicationServices.WorkingDatabase;
+                var id = EntityTools.AddNewEntity(db, pl);
+            }
+            else
+            {
+                ed.WriteMessage("\n输入点过少。");
+            }
+        }
+    }
+}
